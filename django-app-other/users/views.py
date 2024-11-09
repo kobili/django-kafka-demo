bb787e9ef1f115667202becf0239922fb4980333
Kobe Li <likobe23@gmail.com>
2024-11-09 09:30:11 -0800

    Add template kafka sync endpoint

--- django-app-other/users/views.py
+++ django-app-other/users/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from users.models import ExternalUser
 from users.serializers import ExternalUserSerializer
@@ -7,3 +9,8 @@ from users.serializers import ExternalUserSerializer
 class ExternalUserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ExternalUser.objects.all()
     serializer_class = ExternalUserSerializer
+
+    @action(methods=["POST"], detail=False)
+    def kafka_sync(self, *args, **kwargs):
+        # TODO: add kafka consumers here
+        return Response({"message": "would have run kafka consumers"}, status=status.HTTP_200_OK)
