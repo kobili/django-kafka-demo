c98697321010b49a7230b18b7470520b523fa089
Kobe Li <likobe23@gmail.com>
2024-11-14 23:36:30 -0800

    Add kafka app to other django project

---
+++ django-app-other/kafka/client.py
@@ -0,0 +1,35 @@
+import json
+
+from confluent_kafka import Producer
+
+from kafka.models import KafkaProducerLog
+
+
+def _create_delivery_log(err, msg):
+    producer_log_kwargs = {
+        "topic": msg.topic(),
+        "key": msg.key().decode("utf-8"),
+        "value": msg.value().decode("utf-8"),
+    }
+
+    if err:
+        producer_log_kwargs["error_message"] = f"ERROR: Event failed delivery: {err}"
+        producer_log_kwargs["is_success"] = False
+    else:
+        producer_log_kwargs["partition"] = msg.partition()
+        producer_log_kwargs["offset"] = msg.offset()
+
+    KafkaProducerLog.objects.create(**producer_log_kwargs)
+
+
+class KafkaProducer:
+    def __init__(self, kafka_config: dict):
+        self.producer = Producer(kafka_config)
+
+    def produce(self, topic: str, key: str, value: dict | str):
+        if isinstance(value, dict):
+            value = json.dumps(value)
+
+        self.producer.produce(topic, value=value, key=key, callback=_create_delivery_log)
+
+        self.producer.flush()
