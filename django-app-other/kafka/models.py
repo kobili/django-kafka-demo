722f989f10bab97edab04973e405e13bfcaf0ca4
Kobe Li <likobe23@gmail.com>
2024-11-16 12:43:49 -0800

    Add basic kafka consumer logic

--- django-app-other/kafka/models.py
+++ django-app-other/kafka/models.py
@@ -17,3 +17,18 @@ class KafkaProducerLog(models.Model):
     class Meta:
         verbose_name = "Kafka Producer Log"
         verbose_name_plural = "Kafka Producer Logs"
+
+
+class KafkaConsumerLog(models.Model):
+    event_topic = models.CharField(max_length=100, db_index=True)
+    event_key = models.CharField(max_length=100, null=True, blank=True)
+    event_value = models.TextField()
+
+    is_success = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    error_message = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Kafka Consumer Log"
+        verbose_name_plural = "Kafka Consumer Logs"
--- django-app-other/users/management/commands/kafkaconsumer.py
+++ django-app-other/users/management/commands/kafkaconsumer.py
@@ -1,6 +1,10 @@
 import signal
 
 from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from confluent_kafka import Consumer, KafkaError, KafkaException
+from users.constants import KAFKA_TOPIC_USER_UPDATED
 
 
 class Command(BaseCommand):
@@ -14,12 +18,45 @@ class Command(BaseCommand):
         self.running = True
 
     def handle(self, *args, **options):
-        while self.running:
-            # TODO: Consume kafka events here
-            print("running...")
-        
-        # TODO: Perform shutdown events here; cleanup kafka client; commit kafka consumer offsets
-        print("done!")
+        # start kafka consumer
+        consumer = Consumer({
+            "bootstrap.servers": settings.KAFKA_BOOTSTRAP_SERVERS,
+            "group.id": "foo-consumer",
+            "auto.offset.reset": "earliest",
+            'enable.auto.commit': "true",
+            'enable.auto.offset.store': "false",
+        })
+
+        try:
+            consumer.subscribe([KAFKA_TOPIC_USER_UPDATED])
+
+            while self.running:
+                msg = consumer.poll(timeout=1.0)
+
+                if msg is None:
+                    # Continue polling if there are no messages
+                    continue
+
+                if msg.error():
+                    if msg.error().code() == KafkaError.__PARTITION_EOF:
+                        print("No messages in partition")
+                    else:
+                        raise KafkaException(msg.error())
+                
+                else:
+                    # Create/update users
+                    print({
+                        "topic": msg.topic(),
+                        "key": msg.key().decode("utf-8"),
+                        "value": msg.value().decode("utf-8"),
+                    })
+
+        finally:
+            # TODO: Perform shutdown events here; cleanup kafka client; commit kafka consumer offsets
+            print("closing consumer")
+            consumer.close()
+
+            print("done!")
 
     def terminate(self, signum, frame):
         print(f"received signal: {signum}")
